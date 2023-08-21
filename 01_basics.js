8f54ae07cfe6479f89fbd45e85bba0d1b47bca82
itsammarmirza <itsammarmirza@gmail.com>
2023-08-21 11:34:11 +0530

    A little Practice

--- 01_basics.js
+++ 01_basics.js
@@ -1,3 +1,16 @@
 const name = "Ammar"
 console.log(typeof name)
 
+const a = 10;
+let i = 0;
+while (i<a){
+    console.log("Hello")
+    i++;
+}
+
+const obj = {
+    userName : "itsammarmirza",
+    password : "iamnetizen",
+    isLoggedIn : true
+}
+console.log(typeof obj.userName)
