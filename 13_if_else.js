ba68c2db431cf9bcab91f7b3c68c5b1ee58d6f28
itsammarmirza <itsammarmirza@gmail.com>
2023-08-26 23:54:39 +0530

    Some basic things about if else conditionals

---
+++ 13_if_else.js
@@ -0,0 +1,18 @@
+// If and else are conditional statement when we want a code to execute differently on two different conditions
+
+if(condition){
+
+}
+// if condition inside paranthesis will be true the the code will execute
+// If there are only two conditions then you can also use else
+else(condition){
+
+}
+// Only one of them will execute
+
+// you can also use if elseif elseif else for more than two conditions
+
+// You can also add more than on condition in if or else using logical operators
+// && --> code will execute if both the statements are true
+// || --> code will execute if only one of the statements are true
+
