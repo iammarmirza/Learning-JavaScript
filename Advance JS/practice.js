f436f2c208f41b3f191ebb2466da1e7ff2a945bb
itsammarmirza <itsammarmirza@gmail.com>
2023-11-11 22:30:24 +0530

    Did some practice to understand event loop

---
+++ Advance JS/practice.js
@@ -0,0 +1,13 @@
+console.log('Hello')
+
+setTimeout(function(){
+    console.log('Hello 2')
+}, 0)
+
+console.log('Hello 3')
+
+setTimeout(function(){
+    console.log('hello 4')
+}, 0)
+
+console.log('hello 5')
