1aabfdb92f6dfcf701ee2a755e377b30f6c708a0
itsammarmirza <itsammarmirza@gmail.com>
2023-11-13 21:24:25 +0530

    Some more practice on Promise

--- Advance JS/Promise/basics.js
+++ Advance JS/Promise/basics.js
@@ -1,13 +1,23 @@
-let promise = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve('Hello I am resolved')
-    },1000)
-})
+// let promise = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         resolve('Hello I am resolved')
+//     },1000)
+// })
 
-promise.then(function(data){
+// promise.then(function(data){
+//     console.log(data)
+// }).catch(function(error){
+//     console.log(error)
+// }).finally(function(){
+//     console.log('Operation ended')
+// })
+
+Promise.resolve('foo').then((string)=>{
+    let newPromise = new Promise(function(resolve, reject){
+        string += 'baz'
+        resolve(string)
+    })
+}).then((data)=>{
+    data += 'bear'
     console.log(data)
-}).catch(function(error){
-    console.log(error)
-}).finally(function(){
-    console.log('Operation ended')
 })
