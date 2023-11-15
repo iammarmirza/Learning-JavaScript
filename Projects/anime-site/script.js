b6b54f3349fadc7d46adf91aa3daedac96c119e5
itsammarmirza <itsammarmirza@gmail.com>
2023-11-15 13:19:46 +0530

    Added a search button and optimized code further

--- Projects/anime-site/script.js
+++ Projects/anime-site/script.js
@@ -1,4 +1,5 @@
 const searchBox = document.getElementById('searchbox');
+const searchButton = document.getElementById('search-button')
 const cardHolder = document.querySelector('.card-holder')
 const BASE_URL = 'https://api.jikan.moe/v4/anime';
 
@@ -20,38 +21,48 @@ function makeRequest(url) {
     });
 }
 
+function searchAnime() {
+    cardHolder.innerHTML = '';
+    requestUrl = `${BASE_URL}?q=${searchBox.value}`;
+    makeRequest(requestUrl)
+        .then((responseObj) => {
+            return new Promise((resolve, reject) => {
+                const animes = responseObj.data
+                const cards = []
+                for (let i = 0; i < animes.length; i++) {
+                    const { images: { jpg: { image_url } }, title_english, url, title } = animes[i]
+                    const card = document.createElement('div')
+                    card.classList.add('cards');
+                    const cardImage = card.appendChild(document.createElement('img'));
+                    cardImage.classList.add('card-image');
+                    cardImage.setAttribute('src', image_url);
+                    const titleText = card.appendChild(document.createElement('p'));
+                    titleText.classList.add('title');
+                    const animeTitle = title_english || title || "";
+                    if (animeTitle.length < 25) {
+                        titleText.innerText = animeTitle
+                    } else if (animeTitle) {
+                        titleText.innerText = animeTitle.substring(0, 25) + "..."
+                    } else {
+                        titleText.innerText = 'Unknown Title'
+                    }
+                    cards.push(card)
+                }
+                cardHolder.append(...cards)
+            })
+        }).catch(() => {
+            alert('Some Error has occurred');
+        });
+}
+
 searchBox.addEventListener('keypress', function (e) {
     if (e.key === 'Enter' && searchBox.value !== "") {
-        cardHolder.innerHTML = '';
-        requestUrl = `${BASE_URL}?q=${searchBox.value}`;
-        makeRequest(requestUrl)
-            .then((responseObj) => {
-                return new Promise((resolve, reject) => {
-                    const animes = responseObj.data
-                    const cards = []
-                    for (let i = 0; i < animes.length; i++) {
-                        const { images: { jpg: { image_url } }, title_english, url, title } = animes[i]
-                        const card = document.createElement('div')
-                        card.classList.add('cards');
-                        const cardImage = card.appendChild(document.createElement('img'));
-                        cardImage.classList.add('card-image');
-                        cardImage.setAttribute('src', image_url);
-                        const titleText = card.appendChild(document.createElement('p'));
-                        titleText.classList.add('title');
-                        const animeTitle = title_english || title || "";
-                        if (animeTitle.length < 25) {
-                            titleText.innerText = animeTitle
-                        } else if (animeTitle) {
-                            titleText.innerText = animeTitle.substring(0, 25) + "..."
-                        } else {
-                            titleText.innerText = 'Unknown Title'
-                        }
-                        cards.push(card)
-                    }
-                    cardHolder.append(...cards)
-                })
-            }).catch(() => {
-                alert('Some Error has occurred');
-            });
+        searchAnime()
+    }
+});
+
+searchButton.addEventListener('click', function () {
+    if (searchBox.value !== "") {
+        searchAnime()
     }
 });
