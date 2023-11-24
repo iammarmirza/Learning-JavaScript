4dac8924f791b49ba39283faf9c052b36eae1353
itsammarmirza <itsammarmirza@gmail.com>
2023-11-24 23:50:20 +0530

    Chnaged XHR with fetch

--- Projects/anime-site/script.js
+++ Projects/anime-site/script.js
@@ -1,68 +1,56 @@
-const searchBox = document.getElementById('searchbox');
-const searchButton = document.getElementById('search-button')
-const cardHolder = document.querySelector('.card-holder')
-const BASE_URL = 'https://api.jikan.moe/v4/anime';
+const searchBox = document.getElementById("searchbox");
+const searchButton = document.getElementById("search-button");
+const cardHolder = document.querySelector(".card-holder");
+const BASE_URL = "https://api.jikan.moe/v4/anime";
 
-function makeRequest(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onreadystatechange = function () {
-            if (this.readyState === 4) {
-                const data = JSON.parse(this.responseText);
-                if (data) {
-                    resolve(data);
-                } else {
-                    reject(new Error('Invalid API response format'));
-                }
-            }
-        };
-        xhr.send();
-    });
+async function makeRequest(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const animes = await data.data
+    const cards = [];
+    for (let i = 0; i < animes.length; i++) {
+      const { images: { jpg: { image_url } }, title_english, url, title } = animes[i]
+      const card = document.createElement("div");
+      card.classList.add("cards");
+      const cardImage = card.appendChild(document.createElement("img"));
+      cardImage.classList.add("card-image");
+      cardImage.setAttribute("src", image_url);
+      const titleText = card.appendChild(document.createElement("p"));
+      titleText.classList.add("title");
+      const animeTitle = title_english || title || "";
+      if (animeTitle.length < 25) {
+        titleText.innerText = animeTitle;
+      } else if (animeTitle) {
+        titleText.innerText = animeTitle.substring(0, 25) + "...";
+      } else {
+        titleText.innerText = "Unknown Title";
+      }
+      cards.push(card);
+    }
+    cardHolder.append(...cards);
+    if (!response.ok) {
+      throw new Error("Something unexpected happened");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function searchAnime() {
-    cardHolder.innerHTML = '';
-    requestUrl = `${BASE_URL}?q=${searchBox.value}`;
-    makeRequest(requestUrl)
-        .then((responseObj) => {
-            return new Promise((resolve, reject) => {
-                const animes = responseObj.data
-                const cards = []
-                for (let i = 0; i < animes.length; i++) {
-                    const { images: { jpg: { image_url } }, title_english, url, title } = animes[i]
-                    const card = document.createElement('div')
-                    card.classList.add('cards');
-                    const cardImage = card.appendChild(document.createElement('img'));
-                    cardImage.classList.add('card-image');
-                    cardImage.setAttribute('src', image_url);
-                    const titleText = card.appendChild(document.createElement('p'));
-                    titleText.classList.add('title');
-                    const animeTitle = title_english || title || "";
-                    if (animeTitle.length < 25) {
-                        titleText.innerText = animeTitle
-                    } else if (animeTitle) {
-                        titleText.innerText = animeTitle.substring(0, 25) + "..."
-                    } else {
-                        titleText.innerText = 'Unknown Title'
-                    }
-                    cards.push(card)
-                }
-                cardHolder.append(...cards)
-            })
-        }).catch(() => {
-            alert('Some Error has occurred');
-        });
+  cardHolder.innerHTML = "";
+  const requestUrl = `${BASE_URL}?q=${searchBox.value}`;
+  makeRequest(requestUrl)
 }
 
-searchBox.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter' && searchBox.value !== "") {
-        searchAnime()
-    }
+searchBox.addEventListener("keypress", function (e) {
+  if (e.key === "Enter" && searchBox.value !== "") {
+    searchAnime();
+  }
 });
 
-searchButton.addEventListener('click', function () {
-    if (searchBox.value !== "") {
-        searchAnime()
-    }
+searchButton.addEventListener("click", function () {
+  if (searchBox.value !== "") {
+    searchAnime();
+  }
 });
